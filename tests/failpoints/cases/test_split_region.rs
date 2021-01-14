rxdoi/tikv

commit 95d44115be1c8efc68bba64add606a34f3f54e0c

raftstore: handle the race between creating new peer and splitting correctly (#8084)
Signed-off-by: Liqi Geng <gengliqiii@gmail.com>

components/raftstore/src/store/fsm/peer.rs
@@ -29,6 +29,7 @@ use protobuf::Message;
 use raft::eraftpb::{ConfChangeType, MessageType};
 use raft::{self, SnapshotStatus, INVALID_INDEX, NO_LIMIT};
 use raft::{Ready, StateRole};
+use tikv_util::collections::HashMap;
 use tikv_util::mpsc::{self, LooseBoundedSender, Receiver};
 use tikv_util::time::duration_to_sec;
 use tikv_util::worker::{Scheduler, Stopped};
@@ -1465,6 +1466,15 @@ where
             return Ok(None);
         }
 
+        let before_check_snapshot_1_2 = || {
+            fail_point!(
+                "before_check_snapshot_1_2",
+                self.fsm.region_id() == 1 && self.fsm.peer_id() == 2,
+                |_| {}
+            );
+        };
+        before_check_snapshot_1_2();
+
         let region_id = msg.get_region_id();
         let snap = msg.get_message().get_snapshot();
         let key = SnapKey::from_region_snap(region_id, snap);
@@ -1510,6 +1520,7 @@ where
                 );
             }
         }
+
         for region in &meta.pending_snapshot_regions {
             if enc_start_key(region) < snap_enc_end_key &&
                enc_end_key(region) > snap_enc_start_key &&
@@ -1589,6 +1600,25 @@ where
         // Check if snapshot file exists.
         self.ctx.snap_mgr.get_snapshot_for_applying(&key)?;
 
+        // WARNING: The checking code must be above this line.
+        // Now all checking passed.
+
+        if self.fsm.peer.local_first_replicate && !self.fsm.peer.is_initialized() {
+            // If the peer is not initialized and passes the snapshot range check, `is_splitting` flag must
+            // be false.
+            // 1. If `is_splitting` is set to true, then the uninitialized peer is created before split is applied
+            //    and the peer id is the same as split one. So there should be no initialized peer before.
+            // 2. If the peer is also created by splitting, then the snapshot range is not overlapped with
+            //    parent peer. It means leader has applied merge and split at least one time. However,
+            //    the prerequisite of merge includes the initialization of all target peers and source peers,
+            //    which is conflict with 1.
+            let pending_create_peers = self.ctx.pending_create_peers.lock().unwrap();
+            let status = pending_create_peers.get(&region_id).cloned();
+            if status != Some((self.fsm.peer_id(), false)) {
+                drop(pending_create_peers);
+                panic!("{} status {:?} is not expected", self.fsm.peer.tag, status);
+            }
+        }
         meta.pending_snapshot_regions.push(snap_region);
 
         assert!(!meta.atomic_snap_regions.contains_key(&region_id));
@@ -1729,6 +1759,21 @@ where
         }
         if meta.regions.remove(&region_id).is_none() && !merged_by_target {
             panic!("{} meta corruption detected", self.fsm.peer.tag)
+        }
+
+        if self.fsm.peer.local_first_replicate {
+            let mut pending_create_peers = self.ctx.pending_create_peers.lock().unwrap();
+            if is_initialized {
+                assert!(pending_create_peers.get(&region_id).is_none());
+            } else {
+                // If this region's data in `pending_create_peers` is not equal to `(peer_id, false)`,
+                // it means this peer will be replaced by the split one.
+                if let Some(status) = pending_create_peers.get(&region_id) {
+                    if *status == (self.fsm.peer_id(), false) {
+                        pending_create_peers.remove(&region_id);
+                    }
+                }
+            }
         }
 
         // Clear merge related structures.
@@ -1895,10 +1940,14 @@ where
         }
     }
 
-    fn on_ready_split_region(&mut self, derived: metapb::Region, regions: Vec<metapb::Region>) {
+    fn on_ready_split_region(
+        &mut self,
+        derived: metapb::Region,
+        regions: Vec<metapb::Region>,
+        new_split_regions: HashMap<u64, apply::NewSplitPeer>,
+    ) {
         self.register_split_region_check_tick();
-        let mut guard = self.ctx.store_meta.lock().unwrap();
-        let meta: &mut StoreMeta = &mut *guard;
+        let mut meta = self.ctx.store_meta.lock().unwrap();
         let region_id = derived.get_id();
         meta.set_region(&self.ctx.coprocessor_host, derived, &mut self.fsm.peer);
         self.fsm.peer.post_split();
@@ -1947,6 +1996,32 @@ where
                 continue;
             }
 
+            // Create new region
+            let new_split_peer = new_split_regions.get(&new_region.get_id()).unwrap();
+            if new_split_peer.result.is_some() {
+                if let Err(e) = self
+                    .fsm
+                    .peer
+                    .mut_store()
+                    .clear_extra_split_data(enc_start_key(&new_region), enc_end_key(&new_region))
+                {
+                    error!(
+                        "failed to cleanup extra split data, may leave some dirty data";
+                        "region_id" => new_region.get_id(),
+                        "err" => ?e,
+                    );
+                }
+                continue;
+            }
+
+            {
+                let mut pending_create_peers = self.ctx.pending_create_peers.lock().unwrap();
+                assert_eq!(
+                    pending_create_peers.remove(&new_region_id),
+                    Some((new_split_peer.peer_id, true))
+                );
+            }
+
             // Insert new regions and validation
             info!(
                 "insert new region";
@@ -1960,7 +2035,7 @@ where
                 // store. After that, the old snapshot comes, followed with the last split
                 // proposal. After it's applied, the uninitialized peer will be met.
                 // We can remove this uninitialized peer directly.
-                if !r.get_peers().is_empty() {
+                if util::is_region_initialized(r) {
                     panic!(
                         "[region {}] duplicated region {:?} for split region {:?}",
                         new_region_id, r, new_region
@@ -1986,6 +2061,7 @@ where
             let mut replication_state = self.ctx.global_replication_state.lock().unwrap();
             new_peer.peer.init_replication_mode(&mut *replication_state);
             drop(replication_state);
+
             let meta_peer = new_peer.peer.peer.clone();
 
             for p in new_region.get_peers() {
@@ -2647,8 +2723,7 @@ where
             }
         }
 
-        let initialized = !prev_region.get_peers().is_empty();
-        if initialized {
+        if util::is_region_initialized(&prev_region) {
             info!(
                 "region changed after applying snapshot";
                 "region_id" => self.fsm.region_id(),
@@ -2660,9 +2735,17 @@ where
             if prev != Some(region.get_id()) {
                 panic!(
                     "{} meta corrupted, expect {:?} got {:?}",
-                    self.fsm.peer.tag, prev_region, prev
+                    self.fsm.peer.tag, prev_region, prev,
                 );
             }
+        } else if self.fsm.peer.local_first_replicate {
+            // This peer is uninitialized previously.
+            // More accurately, the `RegionLocalState` has been persisted so the data can be removed from `pending_create_peers`.
+            let mut pending_create_peers = self.ctx.pending_create_peers.lock().unwrap();
+            assert_eq!(
+                pending_create_peers.remove(&self.fsm.region_id()),
+                Some((self.fsm.peer_id(), false))
+            );
         }
 
         if let Some(r) = meta
@@ -2704,9 +2787,11 @@ where
                 ExecResult::CompactLog { first_index, state } => {
                     self.on_ready_compact_log(first_index, state)
                 }
-                ExecResult::SplitRegion { derived, regions } => {
-                    self.on_ready_split_region(derived, regions)
-                }
+                ExecResult::SplitRegion {
+                    derived,
+                    regions,
+                    new_split_regions,
+                } => self.on_ready_split_region(derived, regions, new_split_regions),
                 ExecResult::PrepareMerge { region, state } => {
                     self.on_ready_prepare_merge(region, state)
                 }

tests/failpoints/cases/test_split_region.rs
@@ -145,3 +145,141 @@ impl Filter for PrevoteRangeFilter {
         Ok(())
     }
 }
+
+// Test if a peer is created from splitting when another initialized peer with the same
+// region id has already existed. In previous implementation, it can be created and panic
+// will happen because there are two initialized peer with the same region id.
+#[test]
+fn test_split_not_to_split_exist_region() {
+    let mut cluster = new_node_cluster(0, 4);
+    configure_for_merge(&mut cluster);
+    cluster.cfg.raft_store.right_derive_when_split = true;
+    cluster.cfg.raft_store.apply_batch_system.max_batch_size = 1;
+    cluster.cfg.raft_store.apply_batch_system.pool_size = 2;
+    let pd_client = Arc::clone(&cluster.pd_client);
+    pd_client.disable_default_operator();
+
+    let r1 = cluster.run_conf_change();
+    pd_client.must_add_peer(r1, new_peer(2, 2));
+    pd_client.must_add_peer(r1, new_peer(3, 3));
+
+    let mut region_a = pd_client.get_region(b"k1").unwrap();
+    // [-∞, k2), [k2, +∞)
+    //    b         a
+    cluster.must_split(&region_a, b"k2");
+
+    cluster.put(b"k0", b"v0").unwrap();
+    must_get_equal(&cluster.get_engine(3), b"k0", b"v0");
+
+    let region_b = pd_client.get_region(b"k0").unwrap();
+    let peer_b_1 = find_peer(&region_b, 1).cloned().unwrap();
+    cluster.must_transfer_leader(region_b.get_id(), peer_b_1);
+
+    let peer_b_3 = find_peer(&region_b, 3).cloned().unwrap();
+    assert_eq!(peer_b_3.get_id(), 1003);
+    let on_handle_apply_1003_fp = "on_handle_apply_1003";
+    fail::cfg(on_handle_apply_1003_fp, "pause").unwrap();
+    // [-∞, k1), [k1, k2), [k2, +∞)
+    //    c         b          a
+    cluster.must_split(&region_b, b"k1");
+
+    pd_client.must_remove_peer(region_b.get_id(), peer_b_3);
+    pd_client.must_add_peer(region_b.get_id(), new_peer(4, 4));
+
+    let mut region_c = pd_client.get_region(b"k0").unwrap();
+    let peer_c_3 = find_peer(&region_c, 3).cloned().unwrap();
+    pd_client.must_remove_peer(region_c.get_id(), peer_c_3);
+    pd_client.must_add_peer(region_c.get_id(), new_peer(4, 5));
+    // [-∞, k2), [k2, +∞)
+    //     c        a
+    pd_client.must_merge(region_b.get_id(), region_c.get_id());
+
+    region_a = pd_client.get_region(b"k2").unwrap();
+    let peer_a_3 = find_peer(&region_a, 3).cloned().unwrap();
+    pd_client.must_remove_peer(region_a.get_id(), peer_a_3);
+    pd_client.must_add_peer(region_a.get_id(), new_peer(4, 6));
+    // [-∞, +∞)
+    //    c
+    pd_client.must_merge(region_a.get_id(), region_c.get_id());
+
+    region_c = pd_client.get_region(b"k1").unwrap();
+    // [-∞, k2), [k2, +∞)
+    //     d        c
+    cluster.must_split(&region_c, b"k2");
+
+    let peer_c_4 = find_peer(&region_c, 4).cloned().unwrap();
+    pd_client.must_remove_peer(region_c.get_id(), peer_c_4);
+    pd_client.must_add_peer(region_c.get_id(), new_peer(3, 7));
+
+    cluster.put(b"k2", b"v2").unwrap();
+    must_get_equal(&cluster.get_engine(3), b"k2", b"v2");
+
+    fail::remove(on_handle_apply_1003_fp);
+
+    // If peer_c_3 is created, `must_get_none` will fail.
+    must_get_none(&cluster.get_engine(3), b"k0");
+}
+
+// Test if a peer is created from splitting when another initialized peer with the same
+// region id existed before and has been destroyed now.
+#[test]
+fn test_split_not_to_split_exist_tombstone_region() {
+    let mut cluster = new_node_cluster(0, 3);
+    configure_for_merge(&mut cluster);
+    cluster.cfg.raft_store.right_derive_when_split = true;
+    cluster.cfg.raft_store.store_batch_system.max_batch_size = 1;
+    cluster.cfg.raft_store.store_batch_system.pool_size = 2;
+    cluster.cfg.raft_store.apply_batch_system.max_batch_size = 1;
+    cluster.cfg.raft_store.apply_batch_system.pool_size = 2;
+    let pd_client = Arc::clone(&cluster.pd_client);
+    pd_client.disable_default_operator();
+
+    fail::cfg("on_raft_gc_log_tick", "return()").unwrap();
+    let r1 = cluster.run_conf_change();
+
+    pd_client.must_add_peer(r1, new_peer(3, 3));
+
+    assert_eq!(r1, 1);
+    let before_check_snapshot_1_2_fp = "before_check_snapshot_1_2";
+    fail::cfg("before_check_snapshot_1_2", "pause").unwrap();
+    pd_client.must_add_peer(r1, new_peer(2, 2));
+
+    cluster.must_put(b"k1", b"v1");
+    cluster.must_put(b"k2", b"v2");
+
+    let region = pd_client.get_region(b"k1").unwrap();
+    cluster.must_split(&region, b"k2");
+    cluster.must_put(b"k22", b"v22");
+
+    must_get_equal(&cluster.get_engine(2), b"k1", b"v1");
+
+    let left = pd_client.get_region(b"k1").unwrap();
+    let left_peer_2 = find_peer(&left, 2).cloned().unwrap();
+    pd_client.must_remove_peer(left.get_id(), left_peer_2);
+    must_get_none(&cluster.get_engine(2), b"k1");
+
+    let on_handle_apply_2_fp = "on_handle_apply_2";
+    fail::cfg("on_handle_apply_2", "pause").unwrap();
+
+    fail::remove(before_check_snapshot_1_2_fp);
+
+    // Wait for the logs
+    sleep_ms(100);
+
+    // If left_peer_2 can be created, dropping all msg to make it exist.
+    cluster.add_send_filter(IsolationFilterFactory::new(2));
+    // Also don't send check stale msg to PD
+    let peer_check_stale_state_fp = "peer_check_stale_state";
+    fail::cfg(peer_check_stale_state_fp, "return()").unwrap();
+
+    fail::remove(on_handle_apply_2_fp);
+
+    // If value of `k22` is equal to `v22`, the previous split log must be applied.
+    must_get_equal(&cluster.get_engine(2), b"k22", b"v22");
+
+    // If left_peer_2 is created, `must_get_none` will fail.
+    must_get_none(&cluster.get_engine(2), b"k1");
+
+    fail::remove("on_raft_gc_log_tick");
+    fail::remove(peer_check_stale_state_fp);
+}

components/raftstore/src/store/peer.rs
@@ -280,6 +280,9 @@ where
     /// Send to these peers to check whether itself is stale.
     pub check_stale_conf_ver: u64,
     pub check_stale_peers: Vec<metapb::Peer>,
+    /// Whether this peer is created by replication and is the first
+    /// one of this region on local store.
+    pub local_first_replicate: bool,
 
     pub txn_extra_op: Arc<AtomicCell<TxnExtraOp>>,
 }
@@ -369,6 +372,7 @@ where
             replication_sync: false,
             check_stale_conf_ver: 0,
             check_stale_peers: vec![],
+            local_first_replicate: false,
             txn_extra_op: Arc::new(AtomicCell::new(TxnExtraOp::Noop)),
         };
 
@@ -383,7 +387,7 @@ where
     /// Sets commit group to the peer.
     pub fn init_replication_mode(&mut self, state: &mut GlobalReplicationState) {
         debug!("init commit group"; "state" => ?state, "region_id" => self.region_id, "peer_id" => self.peer.id);
-        if !self.get_store().region().get_peers().is_empty() {
+        if self.is_initialized() {
             let version = state.status().get_dr_auto_sync().state_id;
             let gb = state.calculate_commit_group(version, self.get_store().region().get_peers());
             self.raft_group.raft.assign_commit_groups(gb);
@@ -933,6 +937,15 @@ where
             if id == self.peer.get_id() {
                 continue;
             }
+            // The `matched` is 0 only in these two cases:
+            // 1. Current leader hasn't communicated with this peer.
+            // 2. This peer does not exist yet(maybe it is created but not initialized)
+            //
+            // The correctness of region merge depends on the fact that all target peers must exist during merging.
+            // (PD rely on `pending_peers` to check whether all target peers exist)
+            //
+            // So if the `matched` is 0, it must be a pending peer.
+            // It can be ensured because `truncated_index` must be greater than `RAFT_INIT_LOG_INDEX`(5).
             if progress.matched < truncated_idx {
                 if let Some(p) = self.get_peer_from_cache(id) {
                     pending_peers.push(p);

tests/integrations/raftstore/test_split_region.rs
@@ -847,10 +847,12 @@ fn test_node_split_update_region_right_derive() {
 #[test]
 fn test_split_with_epoch_not_match() {
     let mut cluster = new_node_cluster(0, 3);
-    cluster.run();
-    cluster.must_transfer_leader(1, new_peer(1, 1));
     let pd_client = Arc::clone(&cluster.pd_client);
     pd_client.disable_default_operator();
+
+    cluster.run();
+
+    cluster.must_transfer_leader(1, new_peer(1, 1));
 
     // Remove a peer to make conf version become 2.
     pd_client.must_remove_peer(1, new_peer(2, 2));

commit 05b185016c006f3b0dc242264a0d03dbe1a2f0d8

raftstore: fix the region meta inconsistency bug during splitting (#9495)
### What problem does this PR solve?

Issue Number: close https://github.com/tikv/tikv/issues/8783

Problem Summary:
https://github.com/tikv/tikv/issues/8783#issuecomment-708298415
The bug is located in function `on_ready_split_region` which is introduced by https://github.com/tikv/tikv/pull/8084.
https://github.com/tikv/tikv/blob/7c9850040ba58d48f0f1a93345c0392dffb2fc5b/components/raftstore/src/store/fsm/peer.rs#L2084-L2112
Line 2111 `continue` means this region has already existed so it should be skipped.
But line 2087-2090 has already added this region to `region_ranges` which leads to metadata inconsistency.

### What is changed and how it works?

What's Changed:
Fix this bug and add 2 tests.

Run `test_split_not_to_split_existed_tombstone_region ` and `test_split_not_to_split_existed_different_uninitialied_peer` can panic in different place in previous code.

### Related changes

- Need to cherry-pick to the release branch

### Check List <!--REMOVE the items that are not applicable-->

Tests <!-- At least one of them must be included. -->

- Integration test

Side effects

- No

### Release note <!-- bugfixes or new feature need a release note -->
* No release note

components/raftstore/src/store/fsm/peer.rs
@@ -1599,15 +1599,6 @@ where
             return Ok(Either::Right(vec![]));
         }
 
-        let before_check_snapshot_1_2 = || {
-            fail_point!(
-                "before_check_snapshot_1_2",
-                self.fsm.region_id() == 1 && self.fsm.peer_id() == 2,
-                |_| {}
-            );
-        };
-        before_check_snapshot_1_2();
-
         let region_id = msg.get_region_id();
         let snap = msg.get_message().get_snapshot();
         let key = SnapKey::from_region_snap(region_id, snap);
@@ -1617,6 +1608,26 @@ where
         let peer_id = msg.get_to_peer().get_id();
         let snap_enc_start_key = enc_start_key(&snap_region);
         let snap_enc_end_key = enc_end_key(&snap_region);
+
+        let before_check_snapshot_1_2_fp = || -> bool {
+            fail_point!(
+                "before_check_snapshot_1_2",
+                self.fsm.region_id() == 1 && self.store_id() == 2,
+                |_| true
+            );
+            false
+        };
+        let before_check_snapshot_1000_2_fp = || -> bool {
+            fail_point!(
+                "before_check_snapshot_1000_2",
+                self.fsm.region_id() == 1000 && self.store_id() == 2,
+                |_| true
+            );
+            false
+        };
+        if before_check_snapshot_1_2_fp() || before_check_snapshot_1000_2_fp() {
+            return Ok(Either::Left(key));
+        }
 
         if snap_region
             .get_peers()
@@ -2209,23 +2220,22 @@ where
 
         let last_key = enc_end_key(regions.last().unwrap());
         if meta.region_ranges.remove(&last_key).is_none() {
-            panic!("{} original region should exists", self.fsm.peer.tag);
+            panic!("{} original region should exist", self.fsm.peer.tag);
         }
         let last_region_id = regions.last().unwrap().get_id();
         for new_region in regions {
             let new_region_id = new_region.get_id();
 
-            let not_exist = meta
-                .region_ranges
-                .insert(enc_end_key(&new_region), new_region_id)
-                .is_none();
-            assert!(not_exist, "[region {}] should not exists", new_region_id);
-
             if new_region_id == region_id {
+                let not_exist = meta
+                    .region_ranges
+                    .insert(enc_end_key(&new_region), new_region_id)
+                    .is_none();
+                assert!(not_exist, "[region {}] should not exist", new_region_id);
                 continue;
             }
 
-            // Create new region
+            // Check if this new region should be splitted
             let new_split_peer = new_split_regions.get(&new_region.get_id()).unwrap();
             if new_split_peer.result.is_some() {
                 if let Err(e) = self
@@ -2242,6 +2252,7 @@ where
                 continue;
             }
 
+            // Now all checking passed.
             {
                 let mut pending_create_peers = self.ctx.pending_create_peers.lock().unwrap();
                 assert_eq!(
@@ -2313,6 +2324,11 @@ where
 
             new_peer.peer.activate(self.ctx);
             meta.regions.insert(new_region_id, new_region.clone());
+            let not_exist = meta
+                .region_ranges
+                .insert(enc_end_key(&new_region), new_region_id)
+                .is_none();
+            assert!(not_exist, "[region {}] should not exist", new_region_id);
             meta.readers
                 .insert(new_region_id, ReadDelegate::from_peer(new_peer.get_peer()));
             if last_region_id == new_region_id {

tests/failpoints/cases/test_split_region.rs
@@ -248,7 +248,7 @@ impl Filter for PrevoteRangeFilter {
 // region id has already existed. In previous implementation, it can be created and panic
 // will happen because there are two initialized peer with the same region id.
 #[test]
-fn test_split_not_to_split_exist_region() {
+fn test_split_not_to_split_existing_region() {
     let mut cluster = new_node_cluster(0, 4);
     configure_for_merge(&mut cluster);
     cluster.cfg.raft_store.right_derive_when_split = true;
@@ -321,7 +321,7 @@ fn test_split_not_to_split_exist_region() {
 // Test if a peer is created from splitting when another initialized peer with the same
 // region id existed before and has been destroyed now.
 #[test]
-fn test_split_not_to_split_exist_tombstone_region() {
+fn test_split_not_to_split_existing_tombstone_region() {
     let mut cluster = new_node_cluster(0, 3);
     configure_for_merge(&mut cluster);
     cluster.cfg.raft_store.right_derive_when_split = true;
@@ -339,7 +339,7 @@ fn test_split_not_to_split_exist_tombstone_region() {
 
     assert_eq!(r1, 1);
     let before_check_snapshot_1_2_fp = "before_check_snapshot_1_2";
-    fail::cfg("before_check_snapshot_1_2", "pause").unwrap();
+    fail::cfg(before_check_snapshot_1_2_fp, "pause").unwrap();
     pd_client.must_add_peer(r1, new_peer(2, 2));
 
     cluster.must_put(b"k1", b"v1");
@@ -378,8 +378,126 @@ fn test_split_not_to_split_exist_tombstone_region() {
     // If left_peer_2 is created, `must_get_none` will fail.
     must_get_none(&cluster.get_engine(2), b"k1");
 
-    fail::remove("on_raft_gc_log_tick");
-    fail::remove(peer_check_stale_state_fp);
+    cluster.clear_send_filters();
+
+    pd_client.must_add_peer(left.get_id(), new_peer(2, 4));
+
+    must_get_equal(&cluster.get_engine(2), b"k1", b"v1");
+}
+
+// Test if a peer can be created from splitting when another uninitialied peer with the same
+// peer id has been created on this store.
+#[test]
+fn test_split_should_split_existing_same_uninitialied_peer() {
+    let mut cluster = new_node_cluster(0, 3);
+    configure_for_merge(&mut cluster);
+    cluster.cfg.raft_store.right_derive_when_split = true;
+    cluster.cfg.raft_store.store_batch_system.max_batch_size = 1;
+    cluster.cfg.raft_store.store_batch_system.pool_size = 2;
+    cluster.cfg.raft_store.apply_batch_system.max_batch_size = 1;
+    cluster.cfg.raft_store.apply_batch_system.pool_size = 2;
+    let pd_client = Arc::clone(&cluster.pd_client);
+    pd_client.disable_default_operator();
+
+    fail::cfg("on_raft_gc_log_tick", "return()").unwrap();
+    fail::cfg("peer_check_stale_state", "return()").unwrap();
+
+    let r1 = cluster.run_conf_change();
+
+    pd_client.must_add_peer(r1, new_peer(3, 3));
+
+    assert_eq!(r1, 1);
+
+    let before_check_snapshot_1_2_fp = "before_check_snapshot_1_2";
+    fail::cfg(before_check_snapshot_1_2_fp, "pause").unwrap();
+
+    pd_client.must_add_peer(r1, new_peer(2, 2));
+
+    cluster.must_put(b"k1", b"v1");
+    cluster.must_put(b"k2", b"v2");
+
+    let before_check_snapshot_1000_2_fp = "before_check_snapshot_1000_2";
+    fail::cfg(before_check_snapshot_1000_2_fp, "pause").unwrap();
+
+    let region = pd_client.get_region(b"k1").unwrap();
+    cluster.must_split(&region, b"k2");
+    let left = pd_client.get_region(b"k1").unwrap();
+    assert_eq!(left.get_id(), 1000);
+
+    cluster.must_put(b"k11", b"v11");
+
+    // Wait for region 1000 sending heartbeat and snapshot to store 2
+    sleep_ms(200);
+
+    fail::remove(before_check_snapshot_1_2_fp);
+    // peer 2 applied snapshot
+    must_get_equal(&cluster.get_engine(2), b"k1", b"v1");
+
+    fail::remove(before_check_snapshot_1000_2_fp);
+
+    must_get_equal(&cluster.get_engine(2), b"k11", b"v11");
+}
+
+// Test if a peer can be created from splitting when another uninitialied peer with different
+// peer id has been created on this store.
+#[test]
+fn test_split_not_to_split_existing_different_uninitialied_peer() {
+    let mut cluster = new_node_cluster(0, 3);
+    configure_for_merge(&mut cluster);
+    cluster.cfg.raft_store.right_derive_when_split = true;
+    cluster.cfg.raft_store.store_batch_system.max_batch_size = 1;
+    cluster.cfg.raft_store.store_batch_system.pool_size = 2;
+    cluster.cfg.raft_store.apply_batch_system.max_batch_size = 1;
+    cluster.cfg.raft_store.apply_batch_system.pool_size = 2;
+    let pd_client = Arc::clone(&cluster.pd_client);
+    pd_client.disable_default_operator();
+
+    fail::cfg("on_raft_gc_log_tick", "return()").unwrap();
+    let r1 = cluster.run_conf_change();
+
+    pd_client.must_add_peer(r1, new_peer(3, 3));
+
+    assert_eq!(r1, 1);
+
+    let before_check_snapshot_1_2_fp = "before_check_snapshot_1_2";
+    fail::cfg(before_check_snapshot_1_2_fp, "pause").unwrap();
+
+    pd_client.must_add_peer(r1, new_peer(2, 2));
+
+    cluster.must_put(b"k1", b"v1");
+    cluster.must_put(b"k2", b"v2");
+
+    // Wait for region 1 sending heartbeat and snapshot to store 2
+    sleep_ms(200);
+
+    cluster.add_send_filter(IsolationFilterFactory::new(2));
+
+    let region = pd_client.get_region(b"k1").unwrap();
+    cluster.must_split(&region, b"k2");
+    let left = pd_client.get_region(b"k1").unwrap();
+    assert_eq!(left.get_id(), 1000);
+    let left_peer_2 = find_peer(&left, 2).cloned().unwrap();
+
+    pd_client.must_remove_peer(left.get_id(), left_peer_2);
+    pd_client.must_add_peer(left.get_id(), new_peer(2, 4));
+
+    let before_check_snapshot_1000_2_fp = "before_check_snapshot_1000_2";
+    fail::cfg(before_check_snapshot_1000_2_fp, "pause").unwrap();
+
+    cluster.clear_send_filters();
+
+    // Wait for region 1000 sending heartbeat and snapshot to store 2
+    sleep_ms(200);
+
+    fail::remove(before_check_snapshot_1_2_fp);
+
+    // peer 2 applied snapshot
+    must_get_equal(&cluster.get_engine(2), b"k2", b"v2");
+    // But only the right part because there is a peer 4 of region 1000 on local store
+    must_get_none(&cluster.get_engine(2), b"k1");
+
+    fail::remove(before_check_snapshot_1000_2_fp);
+    must_get_equal(&cluster.get_engine(2), b"k1", b"v1");
 }
 
 /// A filter that collects all snapshots.