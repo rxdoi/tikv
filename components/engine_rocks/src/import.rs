rxdoi/tikv

commit b320abc512bd9ff467c6b3da7cd1343215ed9d21

rocks*: add some extra metrics for monitoring the perf of `ingest`. (#17070)
ref tikv/tikv#12362

Add some necessary metrics for the observability on the duration of non-blocking and blocking ingestion of 
external sst files when scaling.

Meanwhile, it also introduces a new metric to observe the duration on the cost of clearing overlapped regions.

Signed-off-by: lucasliang <nkcs_lykx@hotmail.com>

components/engine_rocks/src/import.rs
@@ -6,14 +6,17 @@ use engine_traits::{ImportExt, IngestExternalFileOptions, Result};
 use rocksdb::{
     set_external_sst_file_global_seq_no, IngestExternalFileOptions as RawIngestExternalFileOptions,
 };
+use tikv_util::time::Instant;
 
-use crate::{engine::RocksEngine, r2e, util};
+use crate::{
+    engine::RocksEngine, perf_context_metrics::INGEST_EXTERNAL_FILE_TIME_HISTOGRAM, r2e, util,
+};
 
 impl ImportExt for RocksEngine {
     type IngestExternalFileOptions = RocksIngestExternalFileOptions;
 
-    fn ingest_external_file_cf(&self, cf: &str, files: &[&str]) -> Result<()> {
-        let cf = util::get_cf_handle(self.as_inner(), cf)?;
+    fn ingest_external_file_cf(&self, cf_name: &str, files: &[&str]) -> Result<()> {
+        let cf = util::get_cf_handle(self.as_inner(), cf_name)?;
         let mut opts = RocksIngestExternalFileOptions::new();
         opts.move_files(true);
         opts.set_write_global_seqno(false);
@@ -28,15 +31,28 @@ impl ImportExt for RocksEngine {
                 .map_err(|e| format!("sync {}: {:?}", file, e))
                 .map_err(r2e)
         })?;
+        let now = Instant::now_coarse();
         // This is calling a specially optimized version of
         // ingest_external_file_cf. In cases where the memtable needs to be
         // flushed it avoids blocking writers while doing the flush. The unused
         // return value here just indicates whether the fallback path requiring
         // the manual memtable flush was taken.
-        let _did_nonblocking_memtable_flush = self
+        let did_nonblocking_memtable_flush = self
             .as_inner()
             .ingest_external_file_optimized(cf, &opts.0, files)
             .map_err(r2e)?;
+        let time_cost = now.saturating_elapsed_secs();
+        if did_nonblocking_memtable_flush {
+            INGEST_EXTERNAL_FILE_TIME_HISTOGRAM
+                .get(cf_name.into())
+                .non_block
+                .observe(time_cost);
+        } else {
+            INGEST_EXTERNAL_FILE_TIME_HISTOGRAM
+                .get(cf_name.into())
+                .block
+                .observe(time_cost);
+        }
         Ok(())
     }
 }

components/engine_rocks/src/perf_context_metrics.rs
@@ -21,6 +21,37 @@ make_auto_flush_static_metric! {
     }
 }
 
+make_static_metric! {
+    pub label_enum CFName {
+        default,
+        lock,
+        write,
+        raft
+    }
+
+    pub label_enum IngestType {
+        block,
+        non_block,
+    }
+
+    pub struct IngestExternalFileTimeDuration : Histogram {
+        "cf" => CFName,
+        "type" => IngestType
+    }
+}
+
+impl From<&str> for CFName {
+    fn from(s: &str) -> Self {
+        match s {
+            "DEFAULT" | "default" => CFName::default,
+            "WRITE" | "write" => CFName::write,
+            "LOCK" | "lock" => CFName::lock,
+            "RAFT" | "raft" => CFName::raft,
+            _ => CFName::default,
+        }
+    }
+}
+
 lazy_static! {
     pub static ref APPLY_PERF_CONTEXT_TIME_HISTOGRAM: HistogramVec = register_histogram_vec!(
         "tikv_raftstore_apply_perf_context_time_duration_secs",
@@ -52,4 +83,13 @@ lazy_static! {
         auto_flush_from!(APPLY_PERF_CONTEXT_TIME_HISTOGRAM, PerfContextTimeDuration);
     pub static ref STORE_PERF_CONTEXT_TIME_HISTOGRAM_STATIC: PerfContextTimeDuration =
         auto_flush_from!(STORE_PERF_CONTEXT_TIME_HISTOGRAM, PerfContextTimeDuration);
+    pub static ref INGEST_EXTERNAL_FILE_TIME_HISTOGRAM: IngestExternalFileTimeDuration =
+        register_static_histogram_vec!(
+            IngestExternalFileTimeDuration,
+            "tikv_storage_ingest_external_file_duration_secs",
+            "Bucketed histogram of ingest external file duration.",
+            &["cf", "type"],
+            exponential_buckets(0.005, 2.0, 20).unwrap()
+        )
+        .unwrap();
 }

components/raftstore/src/store/metrics.rs
@@ -719,13 +719,6 @@ lazy_static! {
         "Total number of checking stale peers."
     ).unwrap();
 
-    pub static ref INGEST_SST_DURATION_SECONDS: Histogram =
-        register_histogram!(
-            "tikv_snapshot_ingest_sst_duration_seconds",
-            "Bucketed histogram of rocksdb ingestion durations.",
-            exponential_buckets(0.005, 2.0, 20).unwrap()
-        ).unwrap();
-
     pub static ref RAFT_INVALID_PROPOSAL_COUNTER_VEC: IntCounterVec =
         register_int_counter_vec!(
             "tikv_raftstore_raft_invalid_proposal_total",

components/raftstore/src/store/snap.rs
@@ -1159,7 +1159,6 @@ impl Snapshot {
                     cb,
                 )?;
             } else {
-                let _timer = INGEST_SST_DURATION_SECONDS.start_coarse_timer();
                 let path = cf_file.path.to_str().unwrap(); // path is not used at all
                 let clone_file_paths = cf_file.clone_file_paths();
                 let clone_files = clone_file_paths

components/raftstore/src/store/worker/metrics.rs
@@ -65,6 +65,16 @@ make_static_metric! {
     pub struct LocalReadRejectCounter : LocalIntCounter {
        "reason" => RejectReason,
     }
+
+    pub label_enum ClearOverlapRegionType {
+        by_key,
+        by_range,
+        by_ingest_files,
+    }
+
+    pub struct ClearOverlapRegionDuration : Histogram {
+        "type" => ClearOverlapRegionType,
+    }
 }
 
 pub struct LocalReadMetrics {
@@ -261,4 +271,12 @@ lazy_static! {
         "Total number of renewing lease in advance from local reader."
     )
     .unwrap();
+
+    pub static ref CLEAR_OVERLAP_REGION_DURATION: ClearOverlapRegionDuration = register_static_histogram_vec!(
+        ClearOverlapRegionDuration,
+        "tikv_raftstore_clear_overlap_region_duration_seconds",
+        "Bucketed histogram of clear overlap region duration.",
+        &["type"],
+        exponential_buckets(0.005, 2.0, 20).unwrap()
+    ).unwrap();
 }

components/raftstore/src/store/worker/region.rs
@@ -715,16 +715,27 @@ where
         let wopts = WriteOptions::default();
         for cf in self.engine.cf_names() {
             // CF_LOCK usually contains fewer keys than other CFs, so we delete them by key.
-            let strategy = if cf == CF_LOCK {
-                DeleteStrategy::DeleteByKey
+            let (strategy, observer) = if cf == CF_LOCK {
+                (
+                    DeleteStrategy::DeleteByKey,
+                    &CLEAR_OVERLAP_REGION_DURATION.by_key,
+                )
             } else if self.use_delete_range {
-                DeleteStrategy::DeleteByRange
+                (
+                    DeleteStrategy::DeleteByRange,
+                    &CLEAR_OVERLAP_REGION_DURATION.by_range,
+                )
             } else {
-                DeleteStrategy::DeleteByWriter {
-                    sst_path: self.mgr.get_temp_path_for_ingest(),
-                }
+                (
+                    DeleteStrategy::DeleteByWriter {
+                        sst_path: self.mgr.get_temp_path_for_ingest(),
+                    },
+                    &CLEAR_OVERLAP_REGION_DURATION.by_ingest_files,
+                )
             };
+            let start = Instant::now();
             box_try!(self.engine.delete_ranges_cf(&wopts, cf, strategy, ranges));
+            observer.observe(start.saturating_elapsed_secs());
         }
 
         Ok(())

metrics/grafana/tikv_details.dashboard.py
@@ -737,6 +737,18 @@ def Server() -> RowPanel:
     )
     layout.row(
         [
+            graph_panel_histogram_quantiles(
+                title="Clear overlap region duration",
+                description="Bucketed histogram of clear overlap region duration.",
+                yaxes=yaxes(left_format=UNITS.SECONDS),
+                metric="tikv_raftstore_clear_overlap_region_duration_seconds",
+                by_labels=["type"],
+                hide_count=True,
+            ),
+        ]
+    )
+    layout.row(
+        [
             heatmap_panel(
                 title="Written key size",
                 description="The key size for every put of apply worker",
@@ -5820,10 +5832,11 @@ def RocksDB() -> RowPanel:
             ),
             graph_panel_histogram_quantiles(
                 title="Ingest SST duration seconds",
-                description="The time consumed when ingesting SST files",
+                description="Bucketed histogram of ingest external SST files duration.",
                 yaxes=yaxes(left_format=UNITS.SECONDS),
-                metric="tikv_snapshot_ingest_sst_duration_seconds",
+                metric="tikv_storage_ingest_external_file_duration_secs",
                 label_selectors=['db="$db"'],
+                by_labels=["cf", "type"],
                 hide_count=True,
             ),
         ]

metrics/grafana/tikv_details.json.sha256
@@ -1,1 +1,1 @@
-2579a28eeb2d2d0ff22814fa7a3f53e42c846e306459d09d73c633cacef36bfc  ./metrics/grafana/tikv_details.json
+914c0a5fc983732bef52cecd37c7178df6a5b6f9bcb03616203370762e824fb5  ./metrics/grafana/tikv_details.json

commit 889d9675278fc0232edc351217e8337fcdfc0793

metrics: fix incorrect statistics in `Ingest SST duration seconds`. (#17240)
ref tikv/tikv#17239

This pr is used to fix the incorrect metrics on `Ingest SST duration seconds`, introduced in #17070

Signed-off-by: lucasliang <nkcs_lykx@hotmail.com>

Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

components/engine_rocks/src/import.rs
@@ -21,23 +21,23 @@ impl ImportExt for RocksEngine {
         let now = Instant::now_coarse();
         // This is calling a specially optimized version of
         // ingest_external_file_cf. In cases where the memtable needs to be
-        // flushed it avoids blocking writers while doing the flush. The unused
+        // flushed it avoids blocking writers while doing the flush. The
         // return value here just indicates whether the fallback path requiring
         // the manual memtable flush was taken.
-        let did_nonblocking_memtable_flush = self
+        let did_memtable_flush = self
             .as_inner()
             .ingest_external_file_optimized(cf, &opts.0, files)
             .map_err(r2e)?;
         let time_cost = now.saturating_elapsed_secs();
-        if did_nonblocking_memtable_flush {
+        if did_memtable_flush {
             INGEST_EXTERNAL_FILE_TIME_HISTOGRAM
                 .get(cf_name.into())
-                .non_block
+                .block
                 .observe(time_cost);
         } else {
             INGEST_EXTERNAL_FILE_TIME_HISTOGRAM
                 .get(cf_name.into())
-                .block
+                .non_block
                 .observe(time_cost);
         }
         Ok(())